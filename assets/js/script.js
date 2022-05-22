86839d381416633b176b33fbb479adf8e4e0b3d5
bortkyle18 <bortkyle18@gmail.com>
2022-05-21 23:39:40 -0400

    basic css styling  and sub actor pic

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ var getMovie = function (movieTitle, movieYear) {
                             console.log(data.metacriticRating)
                             console.log(data.plot)
                             console.log(data.actorList)
-                            movieFound(data)
+                            movieFound(data);
                         });
                     })
                     .catch(error => console.log('error', error));
@@ -50,8 +50,8 @@ var movieFound = function(data) {
     var movieInfo = $("<li>");
 
     var image = $("<img>")
-        .attr('src',data.image)
-        .attr('style','width:100%');
+        .attr("src",data.image)
+        .attr("style","width:100%");
     var title = $("<p>")
         .text(data.title);
     var year = $("<p>")
@@ -66,19 +66,39 @@ var movieFound = function(data) {
         .text(data.metacriticRating);
     var plot = $("<p>")
         .text(data.plot);
-    var actors = $("<div>")
+    var actors = $("<div>");
     
-        for(var i = (data.actorList.length-1); i >= 0; i--) {
-            var actorList = $("<li>");
-            
-            var actor = $("<p>")
-                .text(data.actorList[i].name);
+    for(var i = (data.actorList.length-1); i >= 0; i--) {
+        // picture from: https://www.subpng.com/png-bpkbw7/ or https://www.iconfinder.com/icons/445684/award_oscar_round_icon
+        var noImage = "https://imdb-api.com/images/original/nopicture.jpg";
+        var actorList = $("<li>");
+        
+        var actorCard = $("<div>")
+        .addClass("card")
+        .attr("style","width: 18rem;");
+        
+        if (data.actorList[i].image == noImage) {
             var picture = $("<img>")
-                .attr('src',data.actorList[i].image);
-
-            actorList.append(actor,picture);
-            $(actors).append(actorList);
+            .attr("src","./assets/images/oscarIcon.png")
+            .attr("alt","oscar icon, no actor photo available for" + data.actorList[i].name)
+            .addClass("card-img-top");
+        } else {
+            var picture = $("<img>")
+            .attr("src",data.actorList[i].image)
+            .attr("alt",data.actorList[i].name)
+            .addClass("card-img-top");
         };
+        var cardBody = $("<div>")
+            .addClass("card-body");
+        var actor = $("<p>")
+            .addClass("card-text text-center")
+            .text(data.actorList[i].name);
+        cardBody.append(actor);
+        actorCard.append(picture,cardBody);
+
+        actorList.append(actorCard);
+        $(actors).append(actorList);
+    };
 
     movieInfo.append(image,title,year,genres,tagline,imDbRating,metaRating,plot,actors);
     $("#movieInfo").append(movieInfo);
